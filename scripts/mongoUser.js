478a8760fe7738f498dac06264ce1586cefc9be1
ThanFX <ZMikhael@gmail.com>
2015-04-17 21:57:41 +0600

    Create simple testing script

---
+++ scripts/mongoUser.js
@@ -0,0 +1,33 @@
+var mongoose = require('../libs/mongoose');
+var User = require('../models/user').User;
+
+//User.createUser("Vasja", "test123");
+
+var user = new User({
+    login: "Tester",
+    password: "test"
+});
+
+/*User.createUser("Tester", "test", function(err, user){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(user)
+    }
+});*/
+
+/*User.authorize("Tester", "test", function(err, user){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(user)
+    }
+});*/
+
+User.saveJIRAParams("Tester", "test", function(err, user){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(user)
+    }
+});
